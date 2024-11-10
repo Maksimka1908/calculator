cc501a50109f9565b78b0585ec5929aa474ad4fb
Максим <gud.maks999@gmail.com>
2024-11-10 18:01:43 +0300

    HW calculator WEB

---
+++ src/main/java/com/calculator/calculator_code/CalculatorCodeApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.calculator_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorCodeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/calculator/calculator_code/CalculatorController.java
@@ -0,0 +1,6 @@
+package com.calculator.calculator_code;
+
+import org.springframework.web.bind.annotation.RestController;
+
+public class CalculatorController {
+}
